a00313bc49325c8540843c1ec54af999b160f2eb
brt9029 <brt9029@gmail.com>
2022-07-30 19:25:17 -0700

    refactored write/copy file

--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const generateAbout = aboutText => {
     }
 
     return `
-        <section calss="my-3" id="about">
+        <section class="my-3" id="about">
             <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
             <p>${aboutText}</p>
         </section>
